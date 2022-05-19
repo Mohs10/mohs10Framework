33e8293128ee790c18d1849acf006fb0d649f0c2
GayatriMohapatra <101085409+GayatriMohapatra@users.noreply.github.com>
2022-05-19 17:40:58 +0530

    Checkout OR

---
+++ Demo_Open_Cart/src/test/java/com/mohs10/or/Checkoutpage.java
@@ -0,0 +1,13 @@
+package com.gcr.or;
+
+import org.openqa.selenium.By;
+
+public class Checkoutpage {
+	public static By lnkTblets = By.xpath("//a[normalize-space()='Tablets']");
+ 	public static By imgprdct = By.xpath("//img[@title='Samsung Galaxy Tab 10.1']");
+ 	public static By btnAddtocart = By.xpath("//button[@id='button-cart']");
+ 	public static By btnItem =By.xpath("//span[@id='cart-total']");
+ 	public static By txtCart =By.xpath("//strong[normalize-space()='View Cart']");
+ 	public static By btnChckout = By.xpath("//a[@class='btn btn-primary']");
+	
+}
