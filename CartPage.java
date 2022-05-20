337d8484c81852c4ec1cd937aca28bd09dc8f666
SahooSumitra <101085463+SahooSumitra@users.noreply.github.com>
2022-05-20 12:48:38 +0530

    Add files via upload

---
+++ CartPage.java
@@ -0,0 +1,13 @@
+package com.gcr.or;
+
+import org.openqa.selenium.By;
+
+public class CartPage {
+		
+		public static By lnkComapreProductname = By.xpath("//td[@class='a-center']//a[normalize-space()='Smartphone']");
+		public static By lnkComapreProduct1name = By.xpath("//a[normalize-space()='Phone Cover']");
+		public static By btnSmartphone = By.xpath("//td[@class='a-center']//a[normalize-space()='Smartphone']");
+
+	}
+
+
---
+++ HomePage.java
@@ -0,0 +1,16 @@
+package com.gcr.or;
+
+import org.openqa.selenium.By;
+
+public class HomePage {
+	public static By lnkElectronics = By.xpath("//a[@href='/electronics']");
+	public static By lnkCellphone = By.xpath("//a[@href='/cell-phones']");
+	public static By lnksearchbar = By.xpath("//input[@id='small-searchterms']");
+	public static By clksearchbar = By.xpath("//input[@class='button-1 search-box-button']");
+	public static By lnkGiftCards = By.linkText("Gift Cards");
+	public static By lnkjewelry =By.xpath("//a[@href='/jewelry']");
+	public static By lnkBooks = By.linkText("Books");
+	public static By lnkDigitalDownloads  = By .linkText("Digital downloads");
+}
+
+
