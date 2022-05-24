29f8ade9d3a078682baa21de52fd31ea4cece428
SahooSumitra <101085463+SahooSumitra@users.noreply.github.com>
2022-05-24 12:40:46 +0530

    Demo.opencart_Brand_Or_page

---
+++ Demo_Open_Cart/src/test/java/com/mohs10/or/Home_Brand.java
@@ -0,0 +1,22 @@
+package com.gcr.or;
+
+import org.openqa.selenium.By;
+
+public class Home_Brand {
+	public static By lnkMyAccount = By.xpath("//span[normalize-space()='My Account']");
+	public static By lnkLogin = By.xpath("//a[normalize-space()='Login']");
+	public static By txtEmail = By.xpath("//input[@id='input-email']");
+	public static By txtPW = By.xpath("//input[@id='input-password']");
+	public static By btnLogin = By.xpath("//input[@value='Login']");
+	public static By lnkBrand = By.xpath("//a[normalize-space()='Brands']");
+	public static By lnkApple = By.xpath("//a[normalize-space()='Apple']");
+	public static By lnkMacBook = By.xpath("//div[8]//div[1]//div[2]//div[1]//h4[1]//a[1]");
+	public static By btnAddtoCart = By.xpath("//button[@id='button-cart']");
+	public static By lnkCart = By.xpath("//span[@id='cart-total']");
+	public static By lnkViewCart = By.xpath("//strong[normalize-space()='View Cart']");
+	
+			
+	
+			
+
+}
